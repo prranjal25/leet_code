065f8ad8dcc12d5b1c8cf9b951e6f0704678fd1d
prranjal25 <pranjalsharma17720059@gmail.com>
2026-05-25 11:23:12 +0530

    Time: 1591 ms (5.01%) | Memory: 508.2 MB (5.06%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,30 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        /*   sort(nums.begin(), nums.end());
-           int n = nums.size();
-           set<vector<int>> st;
-           vector<vector<int>> ans;
-           for (int i = 0; i < n - 3; i++) {
-               for (int j = i + 1; j < n - 2; j++) {
-                   for (int k = j + 1; k < n - 1; k++) {
-                       for (int l = k + 1; l < n; l++) {
-
-                           long long sum =
-                               (long long)nums[i] + nums[j] + nums[k] + nums[l];
-
-                           if (sum == target) {
-                               st.insert({nums[i], nums[j], nums[k], nums[l]});
-                           }
-                       }
-                   }
-               }
-           }
-
-           for (auto it : st) {
-               ans.push_back(it);
-           }
-           return ans; */  //TIME LIMIT EXCEED
+       
 
         int n = nums.size();
                 vector<vector<int>> ans;
