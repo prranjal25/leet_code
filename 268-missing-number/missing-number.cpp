98d664492be014f5f429517b3003f8869d38bf8b
prranjal25 <pranjalsharma17720059@gmail.com>
2026-06-23 06:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (69.97%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0; int k=0;
+        while(i<nums.size()){
+            if(nums[i]!=i) return i;
+            i++;
+        }
+        return i;
+    }
+};
